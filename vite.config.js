aef91b0755f04d59bfaea13d39e160ee74b161d7
Sebastian Villalobos <dsvillacorta@gmail.com>
2024-11-23 14:55:59 -0600

    Added an scope

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
       manifest: {
         name: "Harry Potter",
         short_name: "Harry Potter",
+        scope: "/",
         description: "Harry Potter Guide",
         theme_color: "#617c94",
         background_color: "#617c94",
